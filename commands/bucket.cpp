34a6dbefa1e6c9fa110eb1d9a077783739b2c9a0
James Meisler <james.meisler@gmail.com>
2025-10-21 09:35:30 -0500

    table stlying

--- commands/bucket.cpp
+++ commands/bucket.cpp
@@ -1,9 +1,39 @@
 //
 // Created by james on 10/20/25.
 //
+#include <ftxui/dom/elements.hpp>  // for color, Fit, LIGHT, align_right, bold, DOUBLE
+#include <ftxui/dom/table.hpp>      // for Table, TableSelection
+#include <ftxui/screen/screen.hpp>  // for Screen
+
+#include "ftxui/dom/node.hpp"  // for Render
+#include "ftxui/screen/color.hpp"  // for Color, Color::Blue, Color::Cyan, Color::White, ftxui
+#include <vector>
+#include <algorithm>
 
 #include "bucket.hpp"
 namespace schema = db::schema;
+
+struct BucketVM {
+    int id;
+    std::string name;
+    std::string projectName;
+    std::string capex;
+};
+
+std::vector<std::vector<std::string>> convertBucketVector(std::vector<BucketVM> src) {
+    std::vector<std::vector<std::string>> dst;
+    dst.reserve(src.size() + 1);
+    dst.emplace_back(std::initializer_list<std::string>{"id", "name", "project name", "capx"});
+    std::ranges::transform(src, std::back_inserter(dst),
+        [](BucketVM bucket) {
+            return std::vector{
+            std::to_string(bucket.id),
+            bucket.name,
+            bucket.projectName,
+            bucket.capex};
+        });
+    return dst;
+}
 
 class BucketNameUniqueValidator : public CLI::Validator {
 public:
@@ -55,8 +85,49 @@ BucketCmdHandles addBucketCommand(CLI::App &app, CLIOptions &opts, AppContext& c
 }
 
 static void runBucketList(const AppContext& ctx) {
+    using namespace ftxui;
+    auto storage = ctx.storage->getStorage();
+    auto rows = storage.select(columns(&schema::Bucket::id,
+        &schema::Bucket::name, &schema::Project::name, &schema::Bucket::capex),
+        inner_join<schema::Project>(on(c(&schema::Bucket::projectId) == &schema::Project::id)));
 
+    std::vector<BucketVM> vms;
+    vms.reserve(rows.size());
+    for (const auto& row : rows) {
+        vms.push_back(BucketVM {
+        std::get<0>(row),
+        std::get<1>(row),
+        std::get<2>(row),
+        std::get<3>(row) ? "yes" : "no"
+        });
+    }
+
+    auto converted = convertBucketVector(vms);
+
+    auto table = Table(converted);
+
+    //style the table
+    table.SelectAll().Border(LIGHT);
+    table.SelectAll().SeparatorVertical(LIGHT);
+    table.SelectRow(0).Border(LIGHT);
+    table.SelectRow(0).DecorateCells(bold);
+    table.SelectRow(0).DecorateCells(color(Color::White));
+    table.SelectAll().DecorateCells(size(WIDTH, GREATER_THAN, 3));
+
+    auto tableEl = table.Render();
+    auto document = vbox({
+        text("Buckets:") | bold,
+        separator(),
+        tableEl,
+
+    });
+
+    auto screen = Screen::Create(Dimension::Fit(document));
+    Render(screen, document);
+    screen.Print();
+    std::cout << std::endl;
 }
+
 
 static void runBucketAdd(const AppContext& ctx, const BucketAddOpts& opts) {
     try {
